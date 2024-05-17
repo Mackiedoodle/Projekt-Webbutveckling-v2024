8969e53cd42db861602c3637416e1359199f8024
Markus <markus.aho@skola.taby.se>
2024-05-17 23:54:24 +0200

    SIsta ändringarna!!

--- script/app.js
+++ script/app.js
@@ -1,6 +1,20 @@
-function displayText() {
-  var text = document.getElementById("textField");
-  text.style.display = "block";
-}
+
+var isToggled = false;
+
+document.getElementById('myButton').addEventListener('click', function() {
+    if (isToggled){
+        this.style.backgroundColor = 'wheat';
+        this.textContent = 'Testa att klicka på mig!!!';
+    }
+    else {
+        this.style.backgroundColor = 'pink';
+        this.textContent = 'Denna knapp gör inget förutom att byta färg :)'
+    }
+
+    isToggled =!isToggled;
+    
+});
+
+
 
 
